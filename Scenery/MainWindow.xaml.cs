b9781ed9cfb52f4724d9ef57e98b0825802cbd79
justNeph <nukz45@gmail.com>
2012-07-30 17:39:02 -0400

    Added new front window to let the user choose the game they wish to use scenery with

--- Scenery/MainWindow.xaml.cs
+++ Scenery/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using SteamWebAPI;
 
 
 namespace Scenery
@@ -27,41 +26,12 @@ namespace Scenery
             
         }
 
-        private void AuthClick(object sender, RoutedEventArgs e)
+        private void ButtonClicked(object sender, RoutedEventArgs e)
         {
-            String name = username.Text;
-            String pw = password.Password;
-            String code = steamguard.Text;
-            SteamAPISession api = new SteamAPISession();
-            SteamAPISession.LoginStatus status;
-            
-            //Lets see if they gave the code already or not
-            if (!String.IsNullOrEmpty(code))
-            {
-                 status = api.Authenticate(name, pw, code);
-            }
-            else
-            {
-                status = api.Authenticate(name, pw);    
-            }
-
-            if (status == SteamAPISession.LoginStatus.SteamGuard)
-            {
-                response.Text = "Steamguard is setup. Please check your email and enter the Steamguard code. ";
-                steamguard.Visibility = Visibility.Visible;
-            }
-
-            if (status == SteamAPISession.LoginStatus.LoginSuccessful)
-            {
-                List<SteamAPISession.Friend> friends = api.GetFriends();
-                int blockedFriends = friends.Count(f => f.blocked == true);
-                response.Text = "You have " + (friends.Count - blockedFriends) + " friends and " + blockedFriends + " fiends!";
-            }
-            else
-            {
-                 response.Text += "Failed to log in!";
-            }
-                
+            this.Hide();
+            SteamLoginWindow login = new SteamLoginWindow();
+            login.Show();
         }
+
     }
 }
---
+++ Scenery/SteamLoginWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SteamWebAPI;
+
+namespace Scenery
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SteamLoginWindow : Window
+    {
+        public SteamLoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthClick(object sender, RoutedEventArgs e)
+        {
+            String name = username.Text;
+            String pw = password.Password;
+            String code = steamguard.Text;
+            SteamAPISession api = new SteamAPISession();
+            SteamAPISession.LoginStatus status;
+
+            //Lets see if they gave the code already or not
+            if (!String.IsNullOrEmpty(code))
+            {
+                status = api.Authenticate(name, pw, code);
+            }
+            else
+            {
+                status = api.Authenticate(name, pw);
+            }
+
+            if (status == SteamAPISession.LoginStatus.SteamGuard)
+            {
+                response.Text = "Steamguard is setup. Please check your email and enter the Steamguard code. ";
+                steamguard.Visibility = Visibility.Visible;
+                steamguard_label.Visibility = Visibility.Visible;
+            }
+
+            if (status == SteamAPISession.LoginStatus.LoginSuccessful)
+            {
+                List<SteamAPISession.Friend> friends = api.GetFriends();
+                int blockedFriends = friends.Count(f => f.blocked == true);
+                response.Text = "You have " + (friends.Count - blockedFriends) + " friends and " + blockedFriends + " fiends!";
+
+                this.Hide();
+            }
+            else
+            {
+                response.Text += "Failed to log in!";
+            }
+
+        }
+    }
+}
